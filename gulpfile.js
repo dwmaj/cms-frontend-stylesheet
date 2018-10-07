fa281af0f7b5088ea7d46e6870fff5cb11ec9749
Christophe Terranova <lepinse@gmail.com>
2018-10-07 11:01:58 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var input = './assets/sass/*.scss';
+var output = './wp-content/themes/heaj';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('sass', function () {
+  return gulp
+    // Find all `.scss` files from the `input` folder
+    .src(input)
+    // Run Sass on those files
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    // Write the resulting CSS in the output folder
+    .pipe(gulp.dest(output));
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Assets builder for Wordpress Theme course.
+
+Basic stylesheet and html for CMS Frontend course.
+Gulp Task to build sass to `style.css`.
+
+## Install
+
+- Copy `gulpfile.js`, `package.json` and `assets` directory to your Wordpress root directory.
+- Run `npm install`
+- Edit `gulpfile.js` and set `output` variable to your theme path.
+
+## Usage
+
+- Run `gulp sass` to build `style.css` in your theme directory.
+- See `assets/reference.html` for HTML demo.
